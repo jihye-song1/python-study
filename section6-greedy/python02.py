8128b99f0ce08fcb25fbf0573cb56746f167fc46
jihye-song1 <rabbit3919@gmail.com>
2024-02-27 23:01:00 +0900

    SECTION6.GREEDY.최대사과의개수

--- section6-greedy/python02.py
+++ section6-greedy/python02.py
@@ -1,5 +1,23 @@
 def solution(list,limit):
     list.sort(key = lambda v : -v[1])  # 내림차순으로 주어진 배열 정렬
+    sum=0
+    count=0
+    flag=0
+
+    for i in range(0,len(list)):
+        k=list[i][0]
+        if flag==1:
+            break
+        for j in range(0,k): # 2 2 3 2
+            count=count+1
+            if count == limit:
+                flag=1
+                sum=sum+list[i][1]
+                break
+            sum=sum+list[i][1]
+            
+    print("sum is ",sum)
+    print("count is ",count)
 
 
 
